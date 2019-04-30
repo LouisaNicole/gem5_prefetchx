gem5/gem5

commit 54c77aa055ee41c8a4d2d500781752804cdfc339

sim-se: add a faux-filesystem
This change introduces the concept of a faux-filesystem.
The faux-filesystem creates a directory structure in m5out
(or whatever output dir the user specifies) where system calls
may be redirected.

This is useful to avoid non-determinism when reading files
with varying path names (e.g., variations from run-to-run if
the simulation is scheduled on a cluster where paths may change).

Also, this changeset allows circumventing host pseudofiles which
have information specific to the host processor (such as cache
hierarchy or processor information). Bypassing host pseudofiles
can be useful when executing runtimes in the absence of an
operating system kernel since runtimes may try to query standard
files (i.e. /proc or /sys) which are not relevant to an
application executing in syscall emulation mode.

Change-Id: I90821b3b403168b904a662fa98b85def1628621c
Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/12119
Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
Maintainer: Jason Lowe-Power <jason@lowepower.com>
Tested-by: kokoro <noreply+kokoro@google.com>

src/sim/process.cc
@@ -50,6 +50,7 @@
 #include <unistd.h>
 
 #include <array>
+#include <climits>
 #include <csignal>
 #include <map>
 #include <string>
@@ -67,6 +68,7 @@
 #include "sim/emul_driver.hh"
 #include "sim/fd_array.hh"
 #include "sim/fd_entry.hh"
+#include "sim/redirect_path.hh"
 #include "sim/syscall_desc.hh"
 #include "sim/system.hh"
 
@@ -101,6 +103,14 @@
 using namespace std;
 using namespace TheISA;
 
+static std::string
+normalize(std::string& directory)
+{
+    if (directory.back() != '/')
+        directory += '/';
+    return directory;
+}
+
 Process::Process(ProcessParams *params, EmulationPageTable *pTable,
                  ObjectFile *obj_file)
     : SimObject(params), system(params->system),
@@ -111,8 +121,10 @@ Process::Process(ProcessParams *params, EmulationPageTable *pTable,
       initVirtMem(system->getSystemPort(), this,
                   SETranslatingPortProxy::Always),
       objFile(obj_file),
-      argv(params->cmd), envp(params->env), cwd(params->cwd),
+      argv(params->cmd), envp(params->env),
       executable(params->executable),
+      tgtCwd(normalize(params->cwd)),
+      hostCwd(checkPathRedirect(tgtCwd)),
       _uid(params->uid), _euid(params->euid),
       _gid(params->gid), _egid(params->egid),
       _pid(params->pid), _ppid(params->ppid),
@@ -441,6 +453,42 @@ Process::findDriver(std::string filename)
     return nullptr;
 }
 
+std::string
+Process::checkPathRedirect(const std::string &filename)
+{
+    // If the input parameter contains a relative path, convert it. Note,
+    // the return value for this method should always return an absolute
+    // path on the host filesystem. The return value will be used to
+    // open and manipulate the path specified by the input parameter. Since
+    // all filesystem handling in syscall mode is passed through to the host,
+    // we deal only with host paths.
+    auto host_fs_abs_path = absolutePath(filename, true);
+
+    for (auto path : system->redirectPaths) {
+        // Search through the redirect paths to see if a starting substring of
+        // our path falls into any buckets which need to redirected.
+        if (startswith(host_fs_abs_path, path->appPath())) {
+            std::string tail = host_fs_abs_path.substr(path->appPath().size());
+
+            // If this path needs to be redirected, search through a list
+            // of targets to see if we can match a valid file (or directory).
+            for (auto host_path : path->hostPaths()) {
+                if (access((host_path + tail).c_str(), R_OK) == 0) {
+                    // Return the valid match.
+                    return host_path + tail;
+                }
+            }
+            // The path needs to be redirected, but the file or directory
+            // does not exist on the host filesystem. Return the first
+            // host path as a default.
+            return path->hostPaths()[0] + tail;
+        }
+    }
+
+    // The path does not need to be redirected.
+    return host_fs_abs_path;
+}
+
 void
 Process::updateBias()
 {
@@ -487,6 +535,33 @@ Process::getStartPC()
     ObjectFile *interp = getInterpreter();
 
     return interp ? interp->entryPoint() : objFile->entryPoint();
+}
+
+std::string
+Process::absolutePath(const std::string &filename, bool host_filesystem)
+{
+    if (filename.empty() || startswith(filename, "/"))
+        return filename;
+
+    // Verify that the current working directories are initialized properly.
+    // These members should be set initially via params from 'Process.py',
+    // although they may change over time depending on what the application
+    // does during simulation.
+    assert(!tgtCwd.empty());
+    assert(!hostCwd.empty());
+
+    // Construct the absolute path given the current working directory for
+    // either the host filesystem or target filesystem. The distinction only
+    // matters if filesystem redirection is utilized in the simulation.
+    auto path_base = host_filesystem ? hostCwd : tgtCwd;
+
+    // Add a trailing '/' if the current working directory did not have one.
+    normalize(path_base);
+
+    // Append the filename onto the current working path.
+    auto absolute_path = path_base + filename;
+
+    return absolute_path;
 }
 
 Process *
@@ -648,18 +723,4 @@ ProcessParams::create()
     if (process == nullptr)
         fatal("Unknown error creating process object.");
     return process;
-}
-
-std::string
-Process::fullPath(const std::string &file_name)
-{
-    if (file_name[0] == '/' || cwd.empty())
-        return file_name;
-
-    std::string full = cwd;
-
-    if (cwd[cwd.size() - 1] != '/')
-        full += '/';
-
-    return full + file_name;
 }

configs/common/FileSystemConfig.py
@@ -1,0 +1,175 @@
+# Copyright (c) 2015 Advanced Micro Devices, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: David Hashe
+
+from __future__ import print_function
+
+import m5
+from m5.objects import *
+from m5.util.convert import *
+
+import operator, os, platform, getpass
+from os import mkdir, makedirs, getpid, listdir, stat, access
+from pwd import getpwuid
+from os.path import join as joinpath
+from os.path import isdir
+from shutil import rmtree, copyfile
+
+def hex_mask(terms):
+    dec_mask = reduce(operator.or_, [2**i for i in terms], 0)
+    return "%08x" % dec_mask
+
+def file_append(path, contents):
+    with open(joinpath(*path), 'a') as f:
+        f.write(str(contents))
+
+def replace_tree(path):
+    if isdir(path):
+        rmtree(path)
+    mkdir(path)
+
+def config_filesystem(options):
+    fsdir = joinpath(m5.options.outdir, 'fs')
+    replace_tree(fsdir)
+
+    # Set up /proc
+    procdir = joinpath(fsdir, 'proc')
+    mkdir(procdir)
+
+    for i in xrange(options.num_cpus):
+        one_cpu = 'processor       : 0\n'                         + \
+                  'vendor_id       : Generic\n'                   + \
+                  'cpu family      : 0\n'                         + \
+                  'model           : 0\n'                         + \
+                  'model name      : Generic\n'                   + \
+                  'stepping        : 0\n'                         + \
+                  'cpu MHz         : %0.3d\n'                       \
+                        % (toFrequency(options.cpu_clock)/mega)   + \
+                  'cache size:     : %dK\n'                         \
+                        % (toMemorySize(options.l2_size)/kibi)    + \
+                  'physical id     : 0\n'                         + \
+                  'siblings        : %s\n'                          \
+                        % options.num_cpus                        + \
+                  'core id         : %d\n'                          \
+                        % i                                       + \
+                  'cpu cores       : %d\n'                          \
+                        % options.num_cpus                        + \
+                  'fpu             : yes\n'                       + \
+                  'fpu exception   : yes\n'                       + \
+                  'cpuid level     : 1\n'                         + \
+                  'wp              : yes\n'                       + \
+                  'flags           : fpu\n'                       + \
+                  'cache alignment : %d\n'                          \
+                        % options.cacheline_size                  + \
+                  '\n'
+        file_append((procdir, 'cpuinfo'), one_cpu)
+
+    file_append((procdir, 'stat'), 'cpu 0 0 0 0 0 0 0\n')
+    for i in xrange(options.num_cpus):
+        file_append((procdir, 'stat'), 'cpu%d 0 0 0 0 0 0 0\n' % i)
+
+    # Set up /sys
+    sysdir = joinpath(fsdir, 'sys')
+    mkdir(sysdir)
+
+    # Set up /sys/devices/system/cpu
+    cpudir = joinpath(sysdir, 'devices', 'system', 'cpu')
+    makedirs(cpudir)
+
+    file_append((cpudir, 'online'), '0-%d' % (options.num_cpus-1))
+    file_append((cpudir, 'possible'), '0-%d' % (options.num_cpus-1))
+
+    # Set up /tmp
+    tmpdir = joinpath(fsdir, 'tmp')
+    replace_tree(tmpdir)
+
+def register_node(cpu_list, mem, node_number):
+    nodebasedir = joinpath(m5.options.outdir, 'fs', 'sys', 'devices',
+                           'system', 'node')
+
+    nodedir = joinpath(nodebasedir,'node%d' % node_number)
+    makedirs(nodedir)
+
+    file_append((nodedir, 'cpumap'), hex_mask(cpu_list))
+    file_append((nodedir, 'meminfo'),
+                'Node %d MemTotal: %dkB' % (node_number,
+                toMemorySize(str(mem))/kibi))
+
+def register_cpu(physical_package_id, core_siblings,
+                 core_id, thread_siblings):
+    cpudir = joinpath(m5.options.outdir, 'fs',  'sys', 'devices', 'system',
+                      'cpu', 'cpu%d' % core_id)
+
+    if not isdir(joinpath(cpudir, 'topology')):
+        makedirs(joinpath(cpudir, 'topology'))
+    if not isdir(joinpath(cpudir, 'cache')):
+        makedirs(joinpath(cpudir, 'cache'))
+
+    file_append((cpudir, 'online'), '1')
+    file_append((cpudir, 'topology', 'physical_package_id'),
+                physical_package_id)
+    file_append((cpudir, 'topology', 'core_siblings'),
+                hex_mask(core_siblings))
+    file_append((cpudir, 'topology', 'core_id'), core_id)
+    file_append((cpudir, 'topology', 'thread_siblings'),
+                hex_mask(thread_siblings))
+
+def register_cache(level, idu_type, size, line_size, assoc, cpus):
+    fsdir = joinpath(m5.options.outdir, 'fs')
+    for i in cpus:
+        cachedir = joinpath(fsdir, 'sys', 'devices', 'system', 'cpu',
+                            'cpu%d' % i, 'cache')
+
+        j = 0
+        while isdir(joinpath(cachedir, 'index%d' % j)):
+            j += 1
+        indexdir = joinpath(cachedir, 'index%d' % j)
+        makedirs(indexdir)
+
+        file_append((indexdir, 'level'), level)
+        file_append((indexdir, 'type'), idu_type)
+        file_append((indexdir, 'size'), "%dK" % (toMemorySize(size)/kibi))
+        file_append((indexdir, 'coherency_line_size'), line_size)
+
+        # Since cache size = number of indices * associativity * block size
+        num_sets = toMemorySize(size) / int(assoc) * int(line_size)
+
+        file_append((indexdir, 'number_of_sets'), num_sets)
+        file_append((indexdir, 'physical_line_partition'), '1')
+        file_append((indexdir, 'shared_cpu_map'), hex_mask(cpus))
+
+def redirect_paths(chroot):
+    # Redirect filesystem syscalls from src to the first matching dests
+    redirect_paths = [RedirectPath(app_path = "/proc",
+                          host_paths = ["%s/fs/proc" % m5.options.outdir]),
+                      RedirectPath(app_path = "/sys",
+                          host_paths = ["%s/fs/sys"  % m5.options.outdir]),
+                      RedirectPath(app_path = "/tmp",
+                          host_paths = ["%s/fs/tmp"  % m5.options.outdir]),
+                      RedirectPath(app_path = "/",
+                          host_paths = ["%s"         % chroot])]
+    return redirect_paths

configs/common/Options.py
@@ -359,6 +359,19 @@ def addSEOptions(parser):
                       help="Redirect stdout to a file.")
     parser.add_option("--errout", default="",
                       help="Redirect stderr to a file.")
+    parser.add_option("--chroot", action="store", type="string", default="/",
+                      help="The chroot option allows a user to alter the "    \
+                           "search path for processes running in SE mode. "   \
+                           "Normally, the search path would begin at the "    \
+                           "root of the filesystem (i.e. /). With chroot, "   \
+                           "a user can force the process to begin looking at" \
+                           "some other location (i.e. /home/user/rand_dir)."  \
+                           "The intended use is to trick sophisticated "      \
+                           "software which queries the __HOST__ filesystem "  \
+                           "for information or functionality. Instead of "    \
+                           "finding files on the __HOST__ filesystem, the "   \
+                           "process will find the user's replacment files.")
+
 
 def addFSOptions(parser):
     from .FSConfig import os_types

configs/example/se.py
@@ -64,6 +64,7 @@ from common import CacheConfig
 from common import CpuConfig
 from common import BPConfig
 from common import MemConfig
+from common.FileSystemConfig import redirect_paths, config_filesystem
 from common.Caches import *
 from common.cpu2000 import *
 
@@ -240,6 +241,9 @@ for i in range(np):
         system.cpu[i].branchPred = bpClass()
 
     system.cpu[i].createThreads()
+
+system.redirect_paths = redirect_paths(os.path.expanduser(options.chroot))
+config_filesystem(options)
 
 if options.ruby:
     Ruby.create_system(options, False, system)

src/kern/linux/linux.cc
@@ -88,7 +88,7 @@ std::string
 Linux::etcPasswd(Process *process, ThreadContext *tc)
 {
     return csprintf("gem5-user:x:1000:1000:gem5-user,,,:%s:/bin/bash\n",
-                    process->getcwd());
+                    process->tgtCwd);
 }
 
 std::string

src/sim/Process.py
@@ -29,6 +29,7 @@
 from m5.SimObject import *
 from m5.params import *
 from m5.proxy import *
+from os import getcwd
 
 class Process(SimObject):
     type = 'Process'
@@ -58,7 +59,7 @@ class Process(SimObject):
     executable = Param.String('', "executable (overrides cmd[0] if set)")
     cmd = VectorParam.String("command line (executable plus arguments)")
     env = VectorParam.String([], "environment settings")
-    cwd = Param.String('', "current working directory")
+    cwd = Param.String(getcwd(), "current working directory")
     simpoint = Param.UInt64(0, 'simulation point at which to start simulation')
     drivers = VectorParam.EmulatedDriver([], 'Available emulated drivers')
 

src/sim/RedirectPath.py
@@ -1,0 +1,43 @@
+# Copyright (c) 2015 Advanced Micro Devices, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: David Hashe
+
+from m5.SimObject import SimObject
+from m5.params import *
+from m5.proxy import *
+
+class RedirectPath(SimObject):
+    """ Stores paths for filesystem redirection during syscalls. If a path
+    matches 'appPath', then the syscall is redirected to the first 'hostPath'
+    that contains the non-overlapping portion of the path as a valid file. If
+     there are no hits, then the syscall is redirected to the first value.
+    """
+    type = 'RedirectPath'
+    cxx_header = "sim/redirect_path.hh"
+
+    app_path = Param.String("/", "filesystem path from an app's perspective")
+    host_paths = VectorParam.String(["/"], "file path on host filesystem")

src/sim/SConscript
@@ -38,6 +38,7 @@ SimObject('VoltageDomain.py')
 SimObject('System.py')
 SimObject('DVFSHandler.py')
 SimObject('SubSystem.py')
+SimObject('RedirectPath.py')
 
 Source('arguments.cc')
 Source('async.cc')
@@ -56,6 +57,7 @@ Source('init_signals.cc')
 Source('main.cc', tags='main')
 Source('port.cc')
 Source('python.cc', add_tags='python')
+Source('redirect_path.cc')
 Source('root.cc')
 Source('serialize.cc')
 Source('drain.cc')

LouisaNicole/gem5_prefetchx

commit 290aba1af1f85c4b814209bac3860d30ed71578d

sim-se: bugfix for 54c77aa055e
54c77aa055e introduced a bug which manifests as cyclical
dependency on a member initialization for the Process
class.

The current working directory (cwd) parameter is passed into
Process to initialize both the target and host versions of the
cwd. (The target and host versions may differ if the faux
filesystem is used.) The host cwd init invoked methods which
rely on the host cwd already being initialized. To avoid the
bug, the code will now rely on using the targets cwd version,
but will issue checks against the redirect paths.

Change-Id: I4ab644a3e00737dbf249f5d6faf20a26ceb04248
Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/18448
Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
Maintainer: Jason Lowe-Power <jason@lowepower.com>
Tested-by: kokoro <noreply+kokoro@google.com>

src/sim/process.cc
@@ -456,19 +456,16 @@ Process::findDriver(std::string filename)
 std::string
 Process::checkPathRedirect(const std::string &filename)
 {
-    // If the input parameter contains a relative path, convert it. Note,
-    // the return value for this method should always return an absolute
-    // path on the host filesystem. The return value will be used to
-    // open and manipulate the path specified by the input parameter. Since
-    // all filesystem handling in syscall mode is passed through to the host,
-    // we deal only with host paths.
-    auto host_fs_abs_path = absolutePath(filename, true);
+    // If the input parameter contains a relative path, convert it.
+    // The target version of the current working directory is fine since
+    // we immediately convert it using redirect paths into a host version.
+    auto abs_path = absolutePath(filename, false);
 
     for (auto path : system->redirectPaths) {
         // Search through the redirect paths to see if a starting substring of
         // our path falls into any buckets which need to redirected.
-        if (startswith(host_fs_abs_path, path->appPath())) {
-            std::string tail = host_fs_abs_path.substr(path->appPath().size());
+        if (startswith(abs_path, path->appPath())) {
+            std::string tail = abs_path.substr(path->appPath().size());
 
             // If this path needs to be redirected, search through a list
             // of targets to see if we can match a valid file (or directory).
@@ -486,7 +483,7 @@ Process::checkPathRedirect(const std::string &filename)
     }
 
     // The path does not need to be redirected.
-    return host_fs_abs_path;
+    return abs_path;
 }
 
 void
@@ -543,17 +540,17 @@ Process::absolutePath(const std::string &filename, bool host_filesystem)
     if (filename.empty() || startswith(filename, "/"))
         return filename;
 
-    // Verify that the current working directories are initialized properly.
-    // These members should be set initially via params from 'Process.py',
-    // although they may change over time depending on what the application
-    // does during simulation.
-    assert(!tgtCwd.empty());
-    assert(!hostCwd.empty());
-
     // Construct the absolute path given the current working directory for
     // either the host filesystem or target filesystem. The distinction only
     // matters if filesystem redirection is utilized in the simulation.
-    auto path_base = host_filesystem ? hostCwd : tgtCwd;
+    auto path_base = std::string();
+    if (host_filesystem) {
+        path_base = hostCwd;
+        assert(!hostCwd.empty());
+    } else {
+        path_base = tgtCwd;
+        assert(!tgtCwd.empty());
+    }
 
     // Add a trailing '/' if the current working directory did not have one.
     normalize(path_base);